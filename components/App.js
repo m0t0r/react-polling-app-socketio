fcdbf1ac3e2564700f19957dbbcbfdb79ca0fa68
Vitaly <motor.kodlife@gmail.com>
2015-10-25 17:16:05 -0500

    feat(socket.io): Start real-time connections with socket.io

--- components/App.js
+++ components/App.js
@@ -1,6 +1,18 @@
+'use strict';
+
 var React = require('react');
+var io = require('socket.io-client');
 
 var App = React.createClass({
+  componentWillMount() {
+    this.socket = io('http://localhost:8080');
+    this.socket.on('connect', this.connect);
+  },
+
+  connect() {
+    alert('Connected: ' + this.socket.id);
+  },
+
   render() {
     return (
       <h1>React is ready! ;)</h1>
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+'use strict';
+
 var express = require('express');
 
-
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
-app.listen(8080, console.log('Server is running at \'http://localhost:8080\''));
+io.on('connection', function(socket) {
+  console.log('Connected %s', socket.id);
+});
+
+server.listen(8080, console.log('Server is running at \'http://localhost:8080\''));
